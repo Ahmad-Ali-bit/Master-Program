93b7ed1d66fb4d3899a039819c2a799a68fa82a9
Ahmad-Ali-bit <m5626moham@gmail.com>
2025-01-08 20:04:32 +0500

    Update Pass by value.cpp

--- Pass by value.cpp
+++ Pass by value.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
+int Sum(int a,int b,int sum)
+{
+  return sum = a + b;
+}
 int main()
 {
-  
+  int a,b,sum;
+  cout<<"Enter two number"<<endl;
+  cin>>a>>b;
+  sum = Sum(a,b,sum);
+  cout<<"Sum: "<<sum<<endl;
 }
