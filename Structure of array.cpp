c0d2adc4ad4a9df8b4651ee32c1bf8d87dd7d35b
Ahmad-Ali-bit <m5626moham@gmail.com>
2025-01-08 19:57:54 +0500

    Add files via upload

---
+++ Structure of array.cpp
@@ -0,0 +1,23 @@
+#inclue <iostream>
+using namespace std;
+struct Student
+{
+  string Name
+  int RollNo
+  int ID
+  Char Grade
+
+};
+int main()
+{
+  Student arr[5];
+  for(int i=0;i<5;i++)
+  cout<<"Enter your Name"<<endl;
+  cin>>a[i].name;
+  cout<<"Enter your RollNo"<<endl;
+  cin>>a[i].RollNo;
+  cout<<"Enter your ID"<<endl;
+  cin>>a[i].ID;
+  cout<<"Enter your Grade"<<endl;
+  cin>>a[i].Grade;  
+}
